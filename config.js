7a5bd68fac600c3316139fd56aada9ad4e9ac117
ADTSpreadsheet <2531kaeng@gmail.com>
2025-03-23 22:52:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,11 @@
-require("dotenv").config();
-
 module.exports = {
   lineBot2: {
-    accessToken: process.env.LINE_BOT2_ACCESS_TOKEN,
-    channelSecret: process.env.LINE_BOT2_CHANNEL_SECRET,
+    accessToken: "VcdMebbh7xEnFBj3t58u/vjAOfjBbrelQs0pLGPTUmvrc3wHYjyWhAA98hy/SkWE1Tj4HjRxMzQu0V9eFYXH78QVYfxLftp6uqyzZsLACPZMbXIkjxqyqJPVYbcg507U3TwgUjZh+Y/7zpy/IzmZpQdB04t89/1O/w1cDnyilFU=",
+    channelSecret: "3558642df20f8e7e357c70c5ffd826f4",
   },
-  adminUserId: process.env.ADMIN_USER_ID,
+  adminUserId: "Ub7406c5f05771fb36c32c1b1397539f6",
   supabase: {
-    url: process.env.SUPABASE_URL,
-    key: process.env.SUPABASE_KEY,
+    url: "https://wpxpukbvynxawfxcdroj.supabase.co",
+    key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndweHB1a2J2eW54YXdmeGNkcm9qIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MjM4Njc5MiwiZXhwIjoyMDU3OTYyNzkyfQ.tgeHy_TMIx6UuQLBXDiKYTi8QyeO7fMI7ZSRuEBiUKM"
   },
 };
