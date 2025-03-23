50388a49d2190676a46e06bb9561553edf6d16a3
ADTSpreadsheet <2531kaeng@gmail.com>
2025-03-23 23:58:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const registrationRouter = require("./routes/registration");
-
 dotenv.config();
-
 const app = express();
-const PORT = process.env.PORT || 10000;
-
+const PORT = process.env.PORT;
 app.use(bodyParser.json());
-
 // Routes
 app.use("/webhook2", registrationRouter);
-
-app.listen(PORT, () => {
-  console.log(`[✅] ADT Line Bot 2 is running on port ${PORT}`);
-  console.log(`🌐 Webhook URL: https://line-bot-adt-2.onrender.com/webhook2`);
-});
-
 // Start server
 app.listen(PORT, () => {
   console.log(`[✅] ADT Line Bot 2 is running on port ${PORT}`);
+  console.log(`🌐 Webhook URL: https://line-bot-adt-2.onrender.com/webhook2`);
 });
