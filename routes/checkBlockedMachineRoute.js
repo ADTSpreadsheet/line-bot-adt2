04102e6454fbeed8807b723901f76b907750146d
ADTSpreadsheet <2531kaeng@gmail.com>
2025-04-05 11:33:11 +0700

    Update checkBlockedMachineRoute.js

--- routes/checkBlockedMachineRoute.js
+++ routes/checkBlockedMachineRoute.js
@@ -2,10 +2,8 @@
 
 const express = require('express');
 const router = express.Router();
-
 const { checkBlockedMachine } = require('../controllers/checkBlockedMachineController');
 
-// ✅ POST: ตรวจสอบว่า Machine ID ถูกบล็อคหรือไม่
 router.post('/check-machine-status', checkBlockedMachine);
 
 module.exports = router;
