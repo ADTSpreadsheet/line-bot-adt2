4104373baa61cfbcc27fe17a0a157b7a373d9ebc
ADTSpreadsheet <2531kaeng@gmail.com>
2025-04-15 02:34:37 +0700

    Update webhook2.js

--- routes/webhook2.js
+++ routes/webhook2.js
@@ -2,9 +2,9 @@
 
 const express = require('express');
 const router = express.Router();
-const { handleWebhookFromBot2 } = require('../controllers/lineWebhookBot2');
+const { handleWebhookBot2 } = require('../controllers/lineWebhookBot2');
 
-// 📦 POST จาก LINE → แชท Bot2
-router.post('/webhook2', handleWebhookFromBot2);
+// 📬 POST จาก LINE > เข้า Bot2
+router.post('/webhook2', handleWebhookBot2);
 
 module.exports = router;
