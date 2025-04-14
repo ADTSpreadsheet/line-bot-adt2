3fb8afa2f30ac4d9a50679071ae6e543f524f93f
ADTSpreadsheet <2531kaeng@gmail.com>
2025-04-15 01:41:56 +0700

    Update replyToUserRoutes.js

--- routes/replyToUserRoutes.js
+++ routes/replyToUserRoutes.js
@@ -4,12 +4,13 @@ const express = require('express');
 const router = express.Router();
 
 const { replyToUser } = require('../controllers/replyToUserController');
-const { sendToCustomer } = require('../controllers/sendToCustomerController');
+const { sendMessageToCustomer } = require('../controllers/sendToCustomerController');
+
 
 // ✅ Bot2 ตอบกลับไปยังลูกค้า
 router.post('/router/reply-to-user', replyToUser);
 
 // ✅ Bot2 ส่งข้อความกลับไปยังลูกค้าโดยตรง
-router.post('/send-to-customer', sendToCustomer);
+router.post('/send-to-customer', sendMessageToCustomer);
 
 module.exports = router;
