adf766e9d12cc88781b6fdb87e47c2de35a610f4
ADTSpreadsheet <2531kaeng@gmail.com>
2025-04-23 11:56:40 +0700

    Delete controllers/slipRoutes.js

--- controllers/slipRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { handleSlipSubmission } = require("../controllers/handleSlipSubmission");
-
-// POST /api/slip/submit
-router.post("/submit", handleSlipSubmission);
-
-module.exports = router;
