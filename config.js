1c234115c8c1c4bac44d075853bbd883b5120435
ADTSpreadsheet <2531kaeng@gmail.com>
2025-04-23 11:38:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,19 @@
 module.exports = {
   lineBot2: {
-    accessToken: "pkTLXAp+M+m+7wZ+Wx1j8PzpzN7wpH9UWaPtosU6utEdmQylQjxIalKVX4kPqhTU1Tj4HjRxMzQu0V9eFYXH78QVYfxLftp6uqyzZsLACPb41tdH7MDARHoFBn/QAlLaoQ+PAwOU5tpKXJ6Iq+fWaQdB04t89/1O/w1cDnyilFU=",
-    channelSecret: "3558642df20f8e7e357c70c5ffd826f4",
+    accessToken: process.env.BOT2_ACCESS_TOKEN,
+    channelSecret: process.env.BOT2_CHANNEL_SECRET,
   },
-  adminUserId: "Ub7406c5f05771fb36c32c1b1397539f6",
+
+  lineBot3: {
+    accessToken: process.env.BOT3_ACCESS_TOKEN,
+    channelSecret: process.env.BOT3_CHANNEL_SECRET,
+    targetUserId: process.env.BOT3_TARGET_USER_ID, // คุณตั้ม
+  },
+
+  adminUserId: process.env.ADMIN_LINE_USER_ID, // พี่เก่ง
+
   supabase: {
-    url: "https://wpxpukbvynxawfxcdroj.supabase.co",
-    key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndweHB1a2J2eW54YXdmeGNkcm9qIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MjM4Njc5MiwiZXhwIjoyMDU3OTYyNzkyfQ.tgeHy_TMIx6UuQLBXDiKYTi8QyeO7fMI7ZSRuEBiUKM"
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
   },
 };
