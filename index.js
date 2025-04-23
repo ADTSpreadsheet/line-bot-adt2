273245a3c1fc0edfdb076668857aa31733b73710
ADTSpreadsheet <2531kaeng@gmail.com>
2025-04-23 14:34:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const slipRoutes = require("./routes/slipRoutes");
 
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use('/router', machineRoutes);
 app.use('/router', checkBlockedMachineRoute);
 app.use(replyToUserRoutes);
