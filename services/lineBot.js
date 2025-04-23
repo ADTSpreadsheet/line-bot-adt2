cd303d42fad44e19689b88b9a590322c7cc42510
ADTSpreadsheet <2531kaeng@gmail.com>
2025-04-23 12:03:39 +0700

    Create lineBot.js

---
+++ services/lineBot.js
@@ -0,0 +1,19 @@
+const line = require('@line/bot-sdk');
+
+const lineBot3 = new line.Client({
+  channelAccessToken: process.env.BOT3_ACCESS_TOKEN,
+  channelSecret: process.env.BOT3_CHANNEL_SECRET,
+});
+
+const lineBot2 = new line.Client({
+  channelAccessToken: process.env.BOT2_ACCESS_TOKEN,
+  channelSecret: process.env.BOT2_CHANNEL_SECRET,
+});
+
+const adminUserId = process.env.BOT3_TARGET_USER_ID;
+
+module.exports = {
+  lineBot3,
+  lineBot2,
+  adminUserId,
+};
