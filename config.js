75f54526d69086fced4c4418362c0bd10b3a53b9
ADTSpreadsheet <2531kaeng@gmail.com>
2025-04-23 11:39:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,21 @@
 module.exports = {
+  lineBot1: {
+    accessToken: process.env.BOT1_LINE_CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.BOT1_LINE_CHANNEL_SECRET,
+  },
+
   lineBot2: {
-    accessToken: process.env.BOT2_ACCESS_TOKEN,
-    channelSecret: process.env.BOT2_CHANNEL_SECRET,
+    accessToken: process.env.LINE_BOT2_ACCESS_TOKEN,
+    channelSecret: process.env.LINE_BOT2_CHANNEL_SECRET,
   },
 
   lineBot3: {
     accessToken: process.env.BOT3_ACCESS_TOKEN,
     channelSecret: process.env.BOT3_CHANNEL_SECRET,
-    targetUserId: process.env.BOT3_TARGET_USER_ID, // คุณตั้ม
+    targetUserId: process.env.BOT3_TARGET_USER_ID,
   },
 
-  adminUserId: process.env.ADMIN_LINE_USER_ID, // พี่เก่ง
+  adminUserId: process.env.ADMIN_LINE_USER_ID,
 
   supabase: {
     url: process.env.SUPABASE_URL,
