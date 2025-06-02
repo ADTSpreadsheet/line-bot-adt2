3a61eef7490e9195aab1bd78a9e4f5899a39406d
ADTSpreadsheet <2531kaeng@gmail.com>
2025-06-02 13:39:30 +0700

    Update sendOrderFlex.js

--- controllers/sendOrderFlex.js
+++ controllers/sendOrderFlex.js
@@ -136,7 +136,8 @@ const sendOrderFlex = async (req, res) => {
                   action: {
                     type: 'uri',
                     label: '✅ อนุมัติ',
-                    uri: `https://line-bot-adt.onrender.com/auto-approve?ref_code=${ref_code}&license_no=${license_no}&serial_key=${licenseData.serial_key}`
+                    uri: `https://line-bot-adt.onrender.com/auto-approve?ref_code=${ref_code}&license_no=${license_no}`
+
 
                   }
                 },
