9df177caaded0255676384b94d7bb95733004a9d
ADTSpreadsheet <2531kaeng@gmail.com>
2025-06-03 17:58:59 +0700

    Update starterFlexRoutes.js

--- routes/starterFlexRoutes.js
+++ routes/starterFlexRoutes.js
@@ -6,5 +6,6 @@ const { sendStarterSlipToAdmin } = require('../controllers/submitStarterSlip');
 
 // ✅ POST route จาก API1 → ส่ง Flex ให้ Bot2 (Admin)
 router.post('/notify-admin-slip', sendStarterSlipToAdmin);
+router.post('/notify-user-starter', sendStarterSlipToAdmin);
 
 module.exports = router;
